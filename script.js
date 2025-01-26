46fdcb8b79e0b3f8b88f169a6538dd7a8b57e57a
Shakti Priya <priyashakti68@gmail.com>
2025-01-26 23:55:16 +0530

    add preloader and minor updates

---
+++ script.js
@@ -0,0 +1,8 @@
+const preloader=document.querySelector("[data-preloader]");
+// The data- attributes are custom attributes that are used to store extra information on HTML elements, in this case, it’s just named data-preloader.
+
+window.addEventListener("load",()=>{
+    preloader.classList.add("remove");a
+});
+
+//start from 19:28
