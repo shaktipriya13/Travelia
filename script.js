d6686bf63ac5e34b59a2b5414ef03d5f7ff7f279
Shakti Priya <priyashakti68@gmail.com>
2025-01-29 01:33:20 +0530

    complete Header section

--- script.js
+++ script.js
@@ -5,4 +5,29 @@ window.addEventListener("load",()=>{
     preloader.classList.add("remove");a
 });
 
-//start from 2:00:05
+// add event on multiple events at once
+const addEventOnElements= function(elements,eventType,callback){
+    for (let i = 0,len=elements.length; i <len; i++) {
+        elements[i].addEventListener(eventType,callback);      
+    }
+}
+
+// Navbar toggler for mobile
+const navbar=document.querySelector("[data-navbar]")
+const navTogglers=document.querySelectorAll("[data-nav-toggler]")
+const overlay=document.querySelector("[data-overlay]")
+
+const toggleNav=function(){
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+addEventOnElements(navTogglers,"click",toggleNav);
+
+
+// header
+const header=document.querySelector("[data-header]");
+window.addEventListener("scroll",function(){
+    header.classList[window.scrollY>100?"add":"remove"]("active");
+    // use of this keyword?
+})
